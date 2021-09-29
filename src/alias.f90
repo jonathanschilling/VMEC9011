1bc1c81b3797b0dea0bc878a0ee05574f73d47ae
Jonathan Schilling <jonathan.schilling@ipp.mpg.de>
2021-09-29 20:21:57 +0200

    alias basic formatting

--- src/alias.f90
+++ src/alias.f90
@@ -1,39 +1,60 @@
-        subroutine alias(gcon,zcon,work1,work2,work3,gcs,gsc)
-      include 'name1'
-      include 'name0'
-      include 'name2'
-        real gcon(ns*nzeta,ntheta2),zcon(ns*nzeta,ntheta2),
-     >  gcs(ns,0:nmax,0:mpol1),gsc(ns,0:nmax,0:mpol1),
-     >  work3(ns,nzeta,4),work1(4*ns*nzeta),work2(ns*nzeta,4)
+subroutine alias(gcon, zcon, work1, work2, work3, gcs, gsc)
 
-*************
-*                 BEGIN DE-ALIASING (TRUNCATION OF GCON IN FOURIER-SPACE
-*************
-        do 60 m = 1,mpol1-1
-        do 20 l = 1,4*ns*nzeta
- 20     work1(l) = czero
-        do 25 i = 1,ntheta2
-        do 25 jk = 1,ns*nzeta
-        work2(jk,01) = work2(jk,01) + zcon(jk,i)*cosmui(i,m)
- 25     work2(jk,02) = work2(jk,02) + zcon(jk,i)*sinmu (i,m)
-        do 30 n = 0,nmax
-        fm = faccon(n,m)
-        do 30 k = 1,nzeta
-        do 30 js= 2,ns
-        gcs(js,n,m) =gcs(js,n,m) +fm*tcon(js)*work3(js,k,01)*sinnv(k,n)
- 30     gsc(js,n,m) =gsc(js,n,m) +fm*tcon(js)*work3(js,k,02)*cosnv(k,n)
-*************
-*                 RECONSTRUCT DE-ALIASED GCON
-*************
-        do 40 n = 0,nmax
-        do 40 k = 1,nzeta
-        do 40 js= 2,ns
-        work3(js,k,03) = work3(js,k,03) + gcs(js,n,m)*sinnv(k,n)
- 40     work3(js,k,04) = work3(js,k,04) + gsc(js,n,m)*cosnv(k,n)
-        do 50 i = 1,ntheta2
-        do 50 jk= 1,ns*nzeta
- 50     gcon(jk,i) = gcon(jk,i) + work2(jk,03)*cosmu(i,m)
-     >                          + work2(jk,04)*sinmu(i,m)
- 60     continue
-        return
-        end
+      use stel_kinds, only: dp
+
+      implicit none
+
+      real(kind=dp) :: gcon(ns*nzeta,ntheta2)
+      real(kind=dp) :: zcon(ns*nzeta,ntheta2)
+      real(kind=dp) :: work1(4*ns*nzeta)
+      real(kind=dp) :: work2(ns*nzeta,4)
+      real(kind=dp) :: work3(ns,nzeta,4)
+      real(kind=dp) :: gcs(ns,0:nmax,0:mpol1)
+      real(kind=dp) :: gsc(ns,0:nmax,0:mpol1)
+
+      ! BEGIN DE-ALIASING (TRUNCATION OF GCON IN FOURIER-SPACE
+      do m = 1, mpol1-1 ! --> exclude m=0, mpol-2 (lowest and highest)
+
+        ! clean workspace
+        do l = 1,4*ns*nzeta
+          work1(l) = czero
+        end do
+
+        do i = 1,ntheta2
+          do jk = 1,ns*nzeta
+            work2(jk,01) = work2(jk,01) + zcon(jk,i)*cosmui(i,m)
+            work2(jk,02) = work2(jk,02) + zcon(jk,i)*sinmu (i,m)
+          end do
+        end do
+
+        do n = 0,nmax
+          fm = faccon(n,m)
+          do k = 1,nzeta
+            do js= 2,ns
+              gcs(js,n,m) =gcs(js,n,m) +fm*tcon(js)*work3(js,k,01)*sinnv(k,n)
+              gsc(js,n,m) =gsc(js,n,m) +fm*tcon(js)*work3(js,k,02)*cosnv(k,n)
+            end do
+          end do
+        end do
+
+        !  RECONSTRUCT DE-ALIASED GCON
+        do n = 0,nmax
+          do k = 1,nzeta
+            do js= 2,ns
+              work3(js,k,03) = work3(js,k,03) + gcs(js,n,m)*sinnv(k,n)
+              work3(js,k,04) = work3(js,k,04) + gsc(js,n,m)*cosnv(k,n)
+            end do
+          end do
+        end do
+        do i = 1,ntheta2
+          do jk= 1,ns*nzeta
+            gcon(jk,i) = gcon(jk,i) + work2(jk,03)*cosmu(i,m) &
+                                    + work2(jk,04)*sinmu(i,m)
+          end do
+        end do
+
+      end do ! loop over m = 1, mpol1-1
+
+      return
+
+end

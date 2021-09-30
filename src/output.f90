73ef657729fd2c82994eaf84c0e976de4ccf5ee7
Jonathan Schilling <jonathan.schilling@ipp.mpg.de>
2021-09-30 20:00:36 +0200

    output done

--- src/output.f90
+++ src/output.f90
@@ -1,17 +1,23 @@
-        subroutine output
-      include 'name1'
-      include 'name0'
-      include 'name2'
+subroutine output
 
-        iequi = 1
-        call funct3d
-*       PRINT 10, IJACOB
-        write(3,10)ijacob
- 10     format(/,'  NUMBER OF JACOBIAN RESETS = ',i4)
-*       PRINT 20, TIMER(0),TIMER(1)
-        write(3,20)timer(0),timer(1)
- 20     format (/,
-     >  '  TOTAL COMPUTATIONAL TIME :       ',1pe10.2,' SECONDS',/
-     >  '  TIME IN VACUUM LOOP :            ',1pe10.2,' SECONDS',/)
-        return
-        end
+      use stel_kinds, only: dp
+      use scalars, only: iequi, ijacob
+
+      implicit none
+
+      ! iequi=1 tells some parts of VMEC to compute some extra quantities
+      ! that are only needed for the output files.
+      iequi = 1
+      call funct3d
+
+      PRINT   10,IJACOB
+      write(3,10)ijacob
+ 10   format(/,'  NUMBER OF JACOBIAN RESETS = ',i4)
+
+      PRINT   20,TIMER(0),TIMER(1)
+      write(3,20)timer(0),timer(1)
+ 20   format (/,'  TOTAL COMPUTATIONAL TIME :       ',1pe10.2,' SECONDS',/ &
+                '  TIME IN VACUUM LOOP :            ',1pe10.2,' SECONDS',/)
+
+      return
+end

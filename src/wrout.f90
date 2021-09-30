4f0b12d928bd3e88c9e4a07bd037d9778474ef83
Jonathan Schilling <jonathan.schilling@ipp.mpg.de>
2021-09-30 14:55:58 +0200

    wrout formatted

--- src/wrout.f90
+++ src/wrout.f90
@@ -39,14 +39,14 @@ subroutine wrout(bsq, gsqrt, bsubu, bsubv, bsubs, br, bphi, bz, lu, lv)
       ! set lwouttxt to false to get a binary ("unformatted") wout file
       logical, parameter :: lwouttxt = .true.
 
-      ! THIS SUBROUTINE CREATES THE TEXT FILE 'WOUT'
+      ! THIS SUBROUTINE CREATES THE (TEXT) FILE 'WOUT'
       ! WHICH CONTAINS THE CYLINDRICAL COORDINATE
       ! SPECTRAL COEFFICIENTS RMNC,ZMNS,LMNS (FULL MESH)
 
       if (lwouttxt) then
-        WRITE(8,701)GAM,NFP,NS,MPOL,NMAX,MNMAX,ITFSQ,NITER/100+1
+        WRITE(8,701) GAM, NFP, NS, MPOL, NMAX, MNMAX, ITFSQ, NITER/100+1
       else
-        WRITE(8    )GAM,NFP,NS,MPOL,NMAX,MNMAX,ITFSQ,NITER/100+1
+        WRITE(8    ) GAM, NFP, NS, MPOL, NMAX, MNMAX, ITFSQ, NITER/100+1
       end if
   701 FORMAT(F10.3,7I6)
 
@@ -143,16 +143,14 @@ subroutine wrout(bsq, gsqrt, bsubu, bsubv, bsubs, br, bphi, bz, lu, lv)
         phi(js) = twopi*hs * dsum(js-1, phips(2), 1)
       end do
 
-      fac = abs(bscale)**(gam-c2p0)
-
       do js = 2, ns
         if (lwouttxt) then
           WRITE(8,703) IOTAS(JS), MASS(JS)*FAC, PRES(JS), PHIPS(JS), BUCO(JS), &
-                       bvco(js), phi(js), vp(js), ju(js), jv(js),              &
+                       bvco(js),  phi(js),      vp(js),   ju(js),    jv(js),   &
                        specw(js)
         else
           WRITE(8    ) IOTAS(JS), MASS(JS)*FAC, PRES(JS), PHIPS(JS), BUCO(JS), &
-                       bvco(js), phi(js), vp(js), ju(js), jv(js),              &
+                       bvco(js),  phi(js),      vp(js),   ju(js),    jv(js),   &
                        specw(js)
         end if
       end do
@@ -179,18 +177,18 @@ subroutine wrout(bsq, gsqrt, bsubu, bsubv, bsubs, br, bphi, bz, lu, lv)
       nzskip = 1 + nzeta/6
 
       write(3,70)
- 70   format(/,4x,'ZETA',8x,' Rb ',8x,' Zb ',6x,
-     >'BSQMHDI',5x,'BSQVACI',5x,'BSQMHDF',5x,'BSQVACF',/)
+ 70   format(/,4x,'ZETA',8x,' Rb ',8x,' Zb ',6x, &
+                  'BSQMHDI',5x,'BSQVACI',5x,     &
+                  'BSQMHDF',5x,'BSQVACF',/)
       do l = 1, nzeta, nzskip
-          zeta = real(360*(l-1))/real(nzeta)
+          zeta = real(360*(l-1))/real(nzeta) ! in degrees
+          do k = 1, ntheta2, ntskip
+            lk = l + nzeta*(k-1)
+            nl = ns * lk
 
-          do k=1,ntheta2,ntskip
-            lk=l+nzeta*(k-1)
-            nl = ns*lk
-
-            if(iprint.eq.1)write(3,90)zeta,r1(nl)+r1(nl+nrzt),
-     >      z1(nl)+z1(nl+nrzt),(bsqsav(lk,n)/bscale**2,n=1,3),
-     >      bsqvac(lk)/bscale**2
+            write(3,90) zeta, r1(nl)+r1(nl+nrzt), z1(nl)+z1(nl+nrzt),   & ! location
+                        bsqsav(lk,1)/bscale**2, bsqsav(lk,2)/bscale**2, & ! initial magnetic fields
+                        bsqsav(lk,3)/bscale**2, bsqvac(lk)  /bscale**2    ! final magnetic fields
         end do
       end do
  90   format(1pe10.2,1p6e12.4)
@@ -200,16 +198,18 @@ subroutine wrout(bsq, gsqrt, bsubu, bsubv, bsubs, br, bphi, bz, lu, lv)
                   'BR', 8x,'BPHI', 6x,'BZ',8x,   &
                   'BRv',7x,'BPHIv',5x,'BZv',/)
       do l = 1, nzeta, nzskip
-        zeta = real(360*(l-1))/real(nzeta)
+        zeta = real(360*(l-1))/real(nzeta) ! in degrees
         do k=1,ntheta2,ntskip
           lk=l+nzeta*(k-1)
           nl = ns*lk
 
-          write(3,95) zeta, r1(nl)+r1(nl+nrzt), z1(nl)+z1(nl+nrzt), &
-                     (1.5*br(nl)   - 0.5*br(nl-1))/bscale,
-                     (1.5*bphi(nl) - 0.5*bphi(nl-1))/bscale,
-                     (1.5*bz(nl)   - 0.5*bz(nl-1))/bscale, &
-                     brv(lk)/bscale, bphiv(lk)/bscale, bzv(lk)/bscale
+          ! B from the plasma is extrapolated from the last half-grid point
+          ! (just inside the LCFS) onto the LCFS
+          write(3,95) zeta, r1(nl)+r1(nl+nrzt), z1(nl)+z1(nl+nrzt), & ! location
+                     (1.5*br  (nl) - 0.5*br  (nl-1))/bscale,        & ! B^R   of plasma
+                     (1.5*bphi(nl) - 0.5*bphi(nl-1))/bscale,        & ! B^phi of plasma
+                     (1.5*bz  (nl) - 0.5*bz  (nl-1))/bscale,        & ! B^z   of plasma
+                     brv(lk)/bscale, bphiv(lk)/bscale, bzv(lk)/bscale ! B of vacuum
 
           end do
         end do
@@ -217,24 +217,22 @@ subroutine wrout(bsq, gsqrt, bsubu, bsubv, bsubs, br, bphi, bz, lu, lv)
  95   format(1pe10.2,1p2e12.4,1p6e10.2)
 
       write(3,100)
- 100  format(//,3x,'mb',2x,'nb',9x,'rbc',9x,'zbs',3x,'|B|(s=.5)',
-     >3x,'|B|(s=1.)',6x,'mb',2x,'nb',9x,'rbc',9x,'zbs',3x,
-     >'|B|(s=.5)',3x,'|B|(s=1.)'/)
+ 100  format(//,3x,      'mb',2x,        'nb',9x,       'rbc',9x,   'zbs',3x,  '|B|(s=.5)',3x,'|B|(s=1.)',6x, &
+                         'mb',2x,        'nb',9x,       'rbc',9x,   'zbs',3x,  '|B|(s=.5)',3x,'|B|(s=1.)'/)
       do mn=1,mnmax,2
-        write(3,115)nint(xm(mn)),nint(xn(mn)/nfp),rmnc(mn),zmns(mn),
-     >  bmodmn(mn),bmodmn1(mn),nint(xm(mn+1)),nint(xn(mn+1)/nfp),
-     >  rmnc(mn+1),zmns(mn+1),bmodmn(mn+1),bmodmn1(mn+1)
+        write(3,115) nint(xm(mn  )), nint(xn(mn  )/nfp), rmnc(mn  ), zmns(mn  ), bmodmn(mn  ), bmodmn1(mn  ), &
+                     nint(xm(mn+1)), nint(xn(mn+1)/nfp), rmnc(mn+1), zmns(mn+1), bmodmn(mn+1), bmodmn1(mn+1)
       end do
  115  format(i5,i4,1p4e12.4,3x,i5,i4,1p4e12.4)
 
       write(3,120)
- 120  format(/,3x,'mf',2x,'nf',5x,'potvacs',6x,'mf',2x,'nf',5x,
-     >'potvacs',6x,'mf',2x,'nf',5x,'potvacs'/)
-      do mn=1,mpmax,3
-        write(3,135)nint(xmpot(mn)),nint(xnpot(mn)/nfp),
-     >  potvac(mn)/bscale,nint(xmpot(mn+1)),nint(xnpot(mn+1)/nfp),
-     >  potvac(mn+1)/bscale,nint(xmpot(mn+2)),nint(xnpot(mn+2)/nfp),
-     >  potvac(mn+2)/bscale
+ 120  format(/,3x,'mf',2x,'nf',5x,'potvacs',6x, &
+                  'mf',2x,'nf',5x,'potvacs',6x, &
+                  'mf',2x,'nf',5x,'potvacs'/)
+      do mn = 1, mpmax, 3
+        write(3,135) nint(xmpot(mn  )), nint(xnpot(mn  )/nfp), potvac(mn  )/bscale, &
+                     nint(xmpot(mn+1)), nint(xnpot(mn+1)/nfp), potvac(mn+1)/bscale, &
+                     nint(xmpot(mn+2)), nint(xnpot(mn+2)/nfp), potvac(mn+2)/bscale
       end do
  135  format(i5,i4,1pe12.4,3x,i5,i4,1pe12.4,3x,i5,i4,1pe12.4)
 

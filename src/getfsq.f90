885ab7dd3e0dc393211ea7e3269ad5c82f72b915
Jonathan Schilling <jonathan.schilling@ipp.mpg.de>
2021-09-30 19:12:59 +0200

    getfs done

--- src/getfsq.f90
+++ src/getfsq.f90
@@ -1,14 +1,31 @@
-        subroutine getfsq(gcr,gcz,gnormr,gnormz,gnorm,mprecon)
-      include 'name1'
-      include 'name0'
-      include 'name2'
-        real gcr(ns,0:mnd1,2),gcz(ns,0:mnd1,2)
-        gnormr = czero
-        gnormz = czero
-        jsmax = (ns-1) + mprecon
-        do 10 mn = 0,mnd1
-        do 10 js = 1,jsmax
-        gnormr = gnormr + gnorm*(gcr(js,mn,1)**2 + gcr(js,mn,2)**2)
- 10     gnormz = gnormz + gnorm*(gcz(js,mn,1)**2 + gcz(js,mn,2)**2)
-        return
-        end
+subroutine getfsq(gcr, gcz, gnormr, gnormz, gnorm, mprecon)
+
+      use stel_kinds, only: dp
+      use name0, only: czero
+      use name1, only: mnd1
+      use scalars, only: ns
+
+      implicit none
+
+      real(kind=dp), intent(in)  :: gcr(ns,0:mnd1,2)
+      real(kind=dp), intent(in)  :: gcz(ns,0:mnd1,2)
+      real(kind=dp), intent(out) :: gnormr
+      real(kind=dp), intent(out) :: gnormz
+      real(kind=dp), intent(in)  :: gnorm
+      integer,       intent(in)  :: mprecon
+
+      integer :: jsmax, mn, js
+
+      gnormr = czero
+      gnormz = czero
+
+      jsmax = (ns-1) + mprecon
+      do mn = 0, mnd1
+        do js = 1, jsmax
+          gnormr = gnormr + gnorm * (gcr(js,mn,1)**2 + gcr(js,mn,2)**2)
+          gnormz = gnormz + gnorm * (gcz(js,mn,1)**2 + gcz(js,mn,2)**2)
+        end do
+      end do
+
+      return
+end

55581a63e05ad5f0eb814908c95f3c8247b33e22
Jonathan Schilling <jonathan.schilling@ipp.mpg.de>
2021-10-08 18:18:52 +0200

    docs in forces

--- src/forces.f90
+++ src/forces.f90
@@ -48,19 +48,24 @@ subroutine forces(guu, guv, gvv)
       end do
 
       do l = 1, nrzt+1
+        ! 1
         guus(l)  = guu(l)*shalf(l)
         guvs(l)  = guv(l)*shalf(l)
         gvvs(l)  = gvv(l)*shalf(l)
 
+        ! 2
         armn(l)  = ohs*armn(l)*czmn(l)
         azmn(l)  =-ohs*azmn(l)*czmn(l)
 
+        ! 3
         brmn(l)  = brmn(l)*czmn(l)
         bzmn(l)  =-bzmn(l)*czmn(l)
 
+        ! 4
         bsqr(l)  = czmn(l)/shalf(l)
       end do
 
+      ! 5
       do l = 2, nrzt+1
         l1 = l+nrzt
         armn(l1) = armn(l)*shalf(l)
@@ -69,11 +74,13 @@ subroutine forces(guu, guv, gvv)
         bzmn(l1) = bzmn(l)*shalf(l)
       end do
 
-      ! CONSTRUCT CYLINDRICAL FORCE KERNELS (L=EVEN, L1=ODD COMPONENTS)
+      ! CONSTRUCT CYLINDRICAL (R,Z) FORCE KERNELS (L=EVEN, L1=ODD COMPONENTS)
+      ! even-m force contributions
       do l = 1, nrzt
         l1 = l + nrzt
 
         ! average half-grid quantities onto full grid
+        ! 6
         bsqr(l) = cp25*(bsqr(l) + bsqr(l+1)) ! additional factor of 1/2 ???
         czmn(l) = cp25*(czmn(l) + czmn(l+1)) ! additional factor of 1/2 ???
         guu (l) =  cp5*(guu (l) + guu (l+1))
@@ -85,34 +92,48 @@ subroutine forces(guu, guv, gvv)
 
         ! actually do some MHD
         ! These are the "final" forms of even-m aXmn and bXmn where X=r,z
-        ! crmn contains some intermediate quantity required below
-        armn(l) = armn(l+1) - armn(l) + cp5*(crmn(l)          + crmn(l+1)           ) - (gvv(l)*r1(l) + gvvs(l)*r1(l1)                                )
-        crmn(l) =                       cp5*(crmn(l)*shalf(l) + crmn(l+1)*shalf(l+1))
+        ! 7
+        armn(l) = armn(l+1) - armn(l) + cp5*(crmn(l) + crmn(l+1)) - (gvv(l)*r1(l) + gvvs(l)*r1(l1))
+        ! 8
         azmn(l) = azmn(l+1) - azmn(l)
-        brmn(l) =   z1(l1)*bsqr(l)    + cp5*(brmn(l)          + brmn(l+1)           ) - (guu(l)*ru(l) + guus(l)*ru(l1) + guv(l)*rv(l) + guvs(l)*rv(l1))
-        bzmn(l) =  -r1(l1)*bsqr(l)    + cp5*(bzmn(l)          + bzmn(l+1)           ) - (guu(l)*zu(l) + guus(l)*zu(l1) + guv(l)*zv(l) + guvs(l)*zv(l1))
+
+        ! 9
+        brmn(l) =   z1(l1)*bsqr(l)    + cp5*(brmn(l) + brmn(l+1)) - (guu(l)*ru(l) + guus(l)*ru(l1) + guv(l)*rv(l) + guvs(l)*rv(l1))
+        ! 10
+        bzmn(l) =  -r1(l1)*bsqr(l)    + cp5*(bzmn(l) + bzmn(l+1)) - (guu(l)*zu(l) + guus(l)*zu(l1) + guv(l)*zv(l) + guvs(l)*zv(l1))
+
+        ! crmn contains some intermediate quantity required below
+        ! 11
+        crmn(l) = cp5*(crmn(l)*shalf(l) + crmn(l+1)*shalf(l+1))
       end do
 
+      ! odd-m force contributions
       do l = 1, nrzt
         l1 = l + nrzt
 
         ! s = sqrt(s)*sqrt(s) on the full grid
         s2 = sqrts(l)**2
 
+        ! 12
         guus2 = guu(l)*s2
         guvs2 = guv(l)*s2
         gvvs2 = gvv(l)*s2
 
         ! final "MHD" form of odd-m parts of aXmn, bXmn
+        ! 13
         armn(l1) =      armn(l1+1) - armn(l1)  - zu(l1)*czmn(l) - zu(l)*bsqr(l) + crmn(l) - (gvvs(l)*r1(l) + gvvs2*r1(l1))
         azmn(l1) =      azmn(l1+1) - azmn(l1)  + ru(l1)*czmn(l) + ru(l)*bsqr(l)
 
-        brmn(l1) = cp5*(brmn(l1+1) + brmn(l1)) + z1(l1)*czmn(l)                           - (guus(l)*ru(l) + guus2*ru(l1) + guvs(l)*rv(l) + guvs2*rv(l1))
-        bzmn(l1) = cp5*(bzmn(l1+1) + bzmn(l1)) - r1(l1)*czmn(l)                           - (guus(l)*zu(l) + guus2*zu(l1) + guvs(l)*zv(l) + guvs2*zv(l1))
+        ! 14
+        brmn(l1) = cp5*(brmn(l1+1) + brmn(l1)) + z1(l1)*czmn(l) - (guus(l)*ru(l) + guus2*ru(l1) + guvs(l)*rv(l) + guvs2*rv(l1))
+        bzmn(l1) = cp5*(bzmn(l1+1) + bzmn(l1)) - r1(l1)*czmn(l) - (guus(l)*zu(l) + guus2*zu(l1) + guvs(l)*zv(l) + guvs2*zv(l1))
 
         ! actually build final form of cXmn now that temporary stuff in crmn, czmn is no longer needed
+        ! 15
         crmn(l)  = guv (l)*ru(l) + guvs(l)*ru(l1) + gvv (l)*rv(l) + gvvs(l)*rv(l1)
         crmn(l1) = guvs(l)*ru(l) + guvs2  *ru(l1) + gvvs(l)*rv(l) + gvvs2  *rv(l1)
+
+        ! 16
         czmn(l)  = guv (l)*zu(l) + guvs(l)*zu(l1) + gvv (l)*zv(l) + gvvs(l)*zv(l1)
         czmn(l1) = guvs(l)*zu(l) + guvs2  *zu(l1) + gvvs(l)*zv(l) + gvvs2  *zv(l1)
       end do
